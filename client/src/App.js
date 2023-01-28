dfd4d448367b821c37b5c2a83b47d70f012922c8
Prajakta Bujare <73356971+prajakta192@users.noreply.github.com>
2023-01-28 11:57:14 +0000

    added currency selection

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,34 @@
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
 
 function App() {
+const [currency, setCurrency] = useState('INR');
+const [curSymbol, setCurSymbol] = useState('₹')
+
+
+
+useEffect(() => {
+  if(currency === 'INR') {setCurSymbol('₹')}
+    else if(currency === 'USD') {setCurSymbol('$')}
+  
+},[currency])
+
+
+console.log(currency, curSymbol);
+
   return (
     <BrowserRouter>
       <header>
         <Link to='/'>amazona</Link>
+        <select name='currency' onChange={(e) => {setCurrency(e.target.value)}} >
+          <option value='INR'>INR</option>
+          <option value='USD'>USD</option>
+        </select>
       </header>
       <Routes>
-        <Route path='/' element={<HomePage />} />
+        <Route path='/' element={<HomePage curSymbol={curSymbol}/>} />
         <Route path='/product/:id' element={<ProductPage />} />
       </Routes>
     </BrowserRouter>
