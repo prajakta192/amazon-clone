a5fdd9d2b9ed92ced87324204ef646a5ffc89af3
Prajakta Bujare <73356971+prajakta192@users.noreply.github.com>
2023-01-28 11:55:48 +0000

    changed useState to useReduser  for state mngment

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,30 +1,67 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 //import data from '../config/data';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect} from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 //console.log(data);
 
-const HomePage = () => {
-  const [products, setProducts] = useState([]);
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, products: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+
+const HomePage = ({curSymbol}) => {
+  console.log(curSymbol)
+
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+    products: [],
+    loading: true,
+    error: "",
+  });
+  //const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const fetchProductsData = async () => {
-      const result = await axios.get('/api/products');
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get('/api/products');
       //console.log(result.data);
-      setProducts(result.data);
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
+      }
+      
+      
+      //setProducts(result.data);
     };
     fetchProductsData();
   }, []);
 
-  console.log(products);
+console.log(products);
 
   return (
     <main>
       <h1>Featured Products</h1>
       <section className='products'>
-        {products.map((product) => (
+        {
+        loading?<div>Loading</div>:error?<div>{error}</div>:
+       
+
+        products.map((product) => (
+         
           <div className='product' key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
@@ -34,10 +71,11 @@ const HomePage = () => {
                 <span>{product.name}</span>
               </Link>
               <span>
-                <strong>{product.price}</strong>
+                <strong>{curSymbol} {product.price}</strong>
               </span>
             </div>
           </div>
+        
         ))}
       </section>
     </main>
