f14ff4f09d6117600605b0c2d06a46ed1cf6e639
Prajakta Bujare <73356971+prajakta192@users.noreply.github.com>
2023-01-30 12:04:14 +0000

    functionality for theme change

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,8 @@ function App() {
 const [currency, setCurrency] = useState('INR');
 const [curSymbol, setCurSymbol] = useState('₹')
 
-
+//state for theme
+const [theme, setTheme] = useState('light')
 
 useEffect(() => {
   if(currency === 'INR') {setCurSymbol('₹')}
@@ -15,22 +16,36 @@ useEffect(() => {
   
 },[currency])
 
-
+function toggleTheme() {
+  if(theme === 'light') {setTheme('dark')}
+  else{
+    setTheme('light')
+  }
+}
 console.log(currency, curSymbol);
 
   return (
     <BrowserRouter>
-      <header>
-        <Link to='/'>amazona</Link>
-        <select name='currency' onChange={(e) => {setCurrency(e.target.value)}} >
-          <option value='INR'>INR</option>
-          <option value='USD'>USD</option>
-        </select>
-      </header>
-      <Routes>
-        <Route path='/' element={<HomePage curSymbol={curSymbol}/>} />
-        <Route path='/product/:id' element={<ProductPage />} />
-      </Routes>
+     <main className={`${theme}_theme`}>
+
+<header>
+  <Link to='/'>amazona</Link>
+  <div style={{display:'flex', justifyContent:'space-between'}}>
+
+  <select name='currency' onChange={(e) => {setCurrency(e.target.value)}} >
+    <option value='INR'>INR</option>
+    <option value='USD'>USD</option>
+  </select>
+  <div className="toggle-btn-section"><div className="toggle-checkbox m-vertical-auto">
+    <input className="toggle-btn__input" type="checkbox" name="checkbox" />
+    <button type="button" className="toggle_btn" onClick={toggleTheme}></button></div></div>
+  </div>
+</header>
+<Routes>
+  <Route path='/' element={<HomePage curSymbol={curSymbol}/>} />
+  <Route path='/product/:id' element={<ProductPage />} />
+</Routes>
+</main>
     </BrowserRouter>
   );
 }
