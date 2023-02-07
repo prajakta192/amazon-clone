3a5ad9d4df73365250c114a9351321c3b5c7c1db
Prajakta Bujare <73356971+prajakta192@users.noreply.github.com>
2023-02-07 11:00:13 +0000

    create environment for project publish

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,13 @@ if(product){
 }
 })
 
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/client/build')))
+
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'))
+})
+
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
