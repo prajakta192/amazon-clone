2f800abc1e3386524fc8fce02dd946b420eec3c1
prajakta192 <bujare.prajakta17@gmail.com>
2023-02-28 14:28:00 +0530

    vercel.json

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,13 @@ const express =  require('express');
 //const data =  require('./data');
 const dotenv = require('dotenv').config();
 const mongoose = require('mongoose');
+const path = require('path')
+const cors = require('cors');
 const SeedRouter = require('./routes/SeedRoutes');
 const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoute');
 
+app.use(cors())
 //connecting to mongodb. then create schema
 mongoose.connect(process.env.MONGODB_URI).then(() => {
   console.log('connected to db');
@@ -24,8 +27,24 @@ app.use((err,req,res,next) => {
   res.status(500).send({message: err.message})
 })
 
-const port = 8080;
+app.use(express.static(path.join(__dirname, '../client/build')));
 
+app.get("*", function(_, res){
+  res.sendFile(
+    path.join(__dirname, '../client/build/index.html'),
+    function(err){
+      if(err){
+        res.send(500).send(err)
+      }
+    }
+  )
+})
+
+const port = process.env.PORT;
+if(port){
 app.listen(port, () => {
   console.log(`server is on http://localhost:${port}`);
-});
+})
+}
+
+module.exports = app;
